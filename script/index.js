5ab9a9b097e59cee16e80f854fc325a88fb2a769
fateshadez <givelokca@gmail.com>
2025-10-21 22:10:42 +0300

    Add winning condition and tweak the code

--- script/index.js
+++ script/index.js
@@ -1,47 +1,66 @@
-const colorModeButton = document.querySelector(".color-mode");
 const playerName = document.querySelector(".player-name");
 const playerScore = document.querySelector(".player-score");
 const playerNum = document.querySelector(".player-num");
+const computerName = document.querySelector(".computer-name");
 const computerScore = document.querySelector(".computer-score");
 const computerNum = document.querySelector(".computer-num");
 const generateRandomNumsButton = document.querySelector(
   ".generate-random-nums"
 );
 const drawNoti = document.querySelector(".draw-notification");
-
 playerScore.textContent = 0;
 computerScore.textContent = 0;
-
-let getPlayerName = () => {
-  return prompt("Enter your name, please!");
-};
+let timeoutId;
+let winner;
 
 // Getting player's name
-playerName.textContent = getPlayerName();
-if (playerName.textContent.trim() == "") {
-  playerName.textContent = "Guest"
-}
+const getPlayerName = () => {
+  playerName.textContent = prompt("Enter your name, please!");
+  if (playerName.textContent.trim() == "") {
+    playerName.textContent = "Guest";
+  }
+};
 
-let generateRandomNum = () => {
+const generateRandomNum = () => {
   return Math.floor(Math.random() * 101);
 };
 
-let timeoutId;
+const gameOver = (winner) => {
+  playerScore.textContent = 0;
+  computerScore.textContent = 0;
+  playerNum.textContent = "";
+  computerNum.textContent = "";
+  alert(`${winner} is the winner!`);
+};
 
-generateRandomNumsButton.addEventListener("click", () => {
-  playerNum.textContent = generateRandomNum();
-  //playerNum.textContent = 5;
-  computerNum.textContent = generateRandomNum();
-  //computerNum.textContent = 5;
+const checkStatus = () => {
+  if (playerScore.textContent == 3) {
+    winner = playerName.textContent;
+    gameOver(winner);
+  } else if (computerScore.textContent == 3) {
+    winner = computerName.textContent;
+    gameOver(winner);
+  }
+};
+
+const updateScore = () => {
   if (playerNum.textContent > computerNum.textContent) {
     playerScore.textContent++;
   } else if (playerNum.textContent < computerNum.textContent) {
     computerScore.textContent++;
-  } else {
+  } else if (playerNum.textContent == computerNum.textContent) {
     drawNoti.classList.add("draw-notification-show");
     clearTimeout(timeoutId);
     timeoutId = setTimeout(() => {
-       drawNoti.classList.remove("draw-notification-show");
-     }, 3100);
+      drawNoti.classList.remove("draw-notification-show");
+    }, 3100);
   }
+};
+
+getPlayerName();
+generateRandomNumsButton.addEventListener("click", () => {
+  playerNum.textContent = generateRandomNum();
+  computerNum.textContent = generateRandomNum();
+  updateScore();
+  checkStatus();
 });
